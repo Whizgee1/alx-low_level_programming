20e3ef0b725ae22d2180d394ecd55e0534efc44e
Abdulazeez <azeezgaj@gmail.com>
2022-06-30 06:13:06 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- *  */
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
