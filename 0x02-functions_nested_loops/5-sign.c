1d88daaab31632edfcd5a24fc4bf827f6548422d
Abdulazeez <azeezgaj@gmail.com>
2022-07-05 12:43:56 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
+ */
+int print_sign(int n)
+{
+	int numz;
+
+	if (n > 0)
+	{
+		numz = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		numz = 0;
+		_putchar('0');
+	}
+	else
+	{
+		numz = -1;
+	        _putchar('-');
+	}
+	return (numz);
+}
