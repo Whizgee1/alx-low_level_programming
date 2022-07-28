fb475a9ecfe3871f0b9d54b4fd5e10c0991d5c56
Abdulazeez <azeezgaj@gmail.com>
2022-07-28 06:45:30 -0700

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,15 @@
 #ifndef _main_h_
 #define _main_h_
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
 
