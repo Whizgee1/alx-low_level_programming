cb170a1afdfd312acd3b192d0d85c00a6c68b56e
Abdulazeez <azeezgaj@gmail.com>
2022-08-11 07:19:26 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,45 +1,29 @@
 #include "lists.h"
-
 /**
- * add_node - Function to add new node at the beginning
- * @head: head of the list
- * @str: string data of the list to be added
- * Return: result
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
+
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
 	new->next = *head;
 	*head = new;
+
 	return (*head);
-}
-
-/**
- * _strlen - function to calculate the length of a const char string
- * @s: string to work with
- * Return: length of string.
- */
-
-int _strlen(const char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
 }
